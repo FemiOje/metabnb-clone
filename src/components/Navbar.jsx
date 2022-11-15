a3d23e53eccbd917e5348cb9bc24b41e18a7bdcf
Oluwafemi Ojetokun <ojetokunakinwale@gmail.com>
2022-11-15 10:57:08 +0100

    install: react-router-dom v6

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav>
+        <ul>
+
+            <li>
+                <Link to={/home}>Home</Link>
+            </li>
+            <li>
+                <Link to={/place-to-stay}>Place to stay</Link>
+            </li>
+            <li>
+                <Link to={/nfts}>NFTs</Link>
+            </li>
+            <li>
+                <Link to={/ommunity}>Community</Link>
+            </li>
+        </ul>
+    </nav>
   )
 }
 
