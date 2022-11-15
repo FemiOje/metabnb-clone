f7698b79751ac613ab69d0ae163bf5cab84b9b48
Oluwafemi Ojetokun <ojetokunakinwale@gmail.com>
2022-11-15 11:29:33 +0100

    add: set up routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './routes/Home';
+import ComingSoon from './routes/ComingSoon';
+import PlaceToStay from './routes/PlaceToStay';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="place-to-stay" element={<PlaceToStay />} />
+        <Route path="NFTs" element={<ComingSoon />} />
+        <Route path="community" element={<ComingSoon />} />
+      </Routes>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,19 +5,24 @@ const Navbar = () => {
   return (
     <nav>
         <ul>
+            <li>
+                <Link to='/home'>Home</Link>
+            </li>
+            <li>
+                <Link to='/place-to-stay'>Place to stay</Link>
+            </li>
+            <li>
+                <Link to='/nfts'>NFTs</Link>
+            </li>
+            <li>
+                <Link to='/community'>Community</Link>
+            </li>
 
-            <li>
-                <Link to={/home}>Home</Link>
-            </li>
-            <li>
-                <Link to={/place-to-stay}>Place to stay</Link>
-            </li>
-            <li>
-                <Link to={/nfts}>NFTs</Link>
-            </li>
-            <li>
-                <Link to={/ommunity}>Community</Link>
-            </li>
+            <div className='connect-wallet'>
+                <li>
+                    <Link to={'/connect-wallet'}>Connect Wallet</Link>
+                </li>
+            </div>
         </ul>
     </nav>
   )
