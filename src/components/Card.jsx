e64664f2c2ed0664e49e25556680c2812ff0595c
Oluwafemi Ojetokun <ojetokunakinwale@gmail.com>
2022-11-15 21:42:51 +0100

    fix: home page design

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React from 'react';
+import Rating from '../images/rating.svg';
+import CardImage from '../images/card-image-1.png';
+import '../styles/Card.css';
 
 const Card = () => {
   return (
-    <div>Card</div>
+    <div className='card'>    
+        <img 
+          src={CardImage} 
+          alt="desert-king"
+          className='card-image'
+        />
+        <br />
+        <br />
+      
+      <div className="first-row">
+        <span className='title'>Desert King</span>
+        <span classname='price'>1MBT per night</span>
+      </div>
+
+      <br />
+      
+      <div className="second-row">
+        <span className="distance">2345km away</span>
+        <span className="duration">available for 2 weeks stay</span>
+      </div>
+
+      <br />
+      <br />
+
+      <div className='rating'>
+        <img src={Rating} alt="rating" />
+      </div>
+    </div>
   )
 }
 
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,14 +1,69 @@
-import React from 'react'
+import React from 'react';
+import '../styles/Home.css';
+import HeroImages from '../images/hero-images.png';
+import MBToken from '../images/MBToken.svg';
+import Metamask from '../images/Metamask.svg';
+import OpenSea from '../images/OpenSea.svg';
+import NFTsImage from '../images/nfts-image.png';
+import Card from '../components/Card.jsx';
+
 
 const Home = () => {
+
+  const cardData = [1, 2, 3, 4, 5, 6, 7, 8];
   return (
-    <div>
-      <div className='hero'>
-        <h1>Rent a <span>Place</span> away from <span>Home</span> in the <span>Metaverse</span></h1>
-        <img src="" alt="hero" />
-      </div>
-    </div>
+    <>
+      <section id='hero'>
+        <div className='hero-text'>
+          <h1>Rent a <span>Place</span> away from <span>Home</span> in the <span>Metaverse</span></h1>
+
+          <br />
+
+          <p>
+            We provide you access to luxury and affordable houses in the metaverse, get a chance to turn your imagination to reality at your comfort zone
+          </p>
+        </div>
+
+        <div className="hero-images">
+          <img src={HeroImages} alt="hero" />
+        </div>
+      </section>
+
+
+      <section id='sponsors'>
+        <div className="sponsors-logos">
+          <img src={MBToken} alt="MBToken" />
+          <img src={Metamask} alt="Metamask" />
+          <img src={OpenSea} alt="OpenSea" />
+        </div>
+      </section>
+
+      <section id='collection'>
+        <h1>Inspiration for your next adventure</h1>
+        
+        <div className="all-cards">
+          {cardData.map((card) => {
+            return <Card />
+          })}
+        </div>
+      </section>
+
+      <section id="nfts">
+        <div className="nfts-text">
+          <h1>Metabnb NFTs</h1>
+          <p>Discover our NFT gift cards collection. Loyal customers gets amazing gift cards which are traded as NFTs. These NFTs gives our cutomer access to loads of our exclusive services.</p>
+
+          <button className="learn-more">
+            Learn more
+          </button>
+        </div>
+
+        <div className="nfts-image">
+          <img src={NFTsImage} alt="nfts" />
+        </div>
+      </section>
+    </>
   )
 }
 
-export default Home
+export default Home;
