6d14f5031cb8769b95c4f3c7c479bca414d3cef7
Oluwafemi Ojetokun <ojetokunakinwale@gmail.com>
2022-11-21 09:58:52 +0100

    fix: navbar dropdown

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,46 +5,84 @@ import MetabnbLogo from '../images/metabnb-logo.png';
 
 const Navbar = () => {
     const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
     function toggleDropdown() {
         setIsDropdownOpen((prevValue) => !prevValue)
     }
 
     return (
-        <nav>
-            <div className="metabnb-logo">
-                <img src={MetabnbLogo} alt="metabnb" />
-            </div>
+        <>
+            <nav>
+                <div className="metabnb-logo">
+                    <img src={MetabnbLogo} alt="metabnb" />
+                </div>
 
-            <div className='navbar-links'>
-                <Link to='/'>Home</Link>
-                <Link to='/place-to-stay'>Place to stay</Link>
-                <Link to='/nfts'>NFTs</Link>
-                <Link to='/community'>Community</Link>
-            </div>
-
-            <Link to={'/connect-wallet'} className='connect-wallet'>
-                <button className='connect-wallet-button'>
-                    Connect Wallet
-                </button>
-            </Link>
-
-            <button 
-                className='dropdown-button'
-                onClick={toggleDropdown}
-            >
-                Dropdown
-            </button>
-
-            {isDropdownOpen &&
                 <div className='navbar-links'>
                     <Link to='/'>Home</Link>
                     <Link to='/place-to-stay'>Place to stay</Link>
                     <Link to='/nfts'>NFTs</Link>
                     <Link to='/community'>Community</Link>
                 </div>
-            }
 
-        </nav >
+                <Link to={'/connect-wallet'} className='connect-wallet'>
+                    <button className='connect-wallet-button'>
+                        Connect Wallet
+                    </button>
+                </Link>
+
+                <div className="dropdown">
+                    <button
+                        className='dropdown-button'
+                        onClick={toggleDropdown}
+                    >
+                        Dropdown
+                    </button>
+                </div>
+
+
+            </nav >
+
+            {isDropdownOpen &&
+                <div className='dropdown-links'>
+                    <Link to='/'>
+                        <div
+                            className='dropdown-link'
+                            onClick={toggleDropdown}
+                        >
+                            Home
+                        </div>
+                    </Link>
+
+                    <Link to='/place-to-stay'>
+                        <div
+                            className='dropdown-link'
+                            onClick={toggleDropdown}
+                        >
+                            Place to stay
+                        </div>
+                    </Link>
+
+                    <Link to='/nfts'>
+                        <div
+                            className='dropdown-link'
+                            onClick={toggleDropdown}
+                        >
+                            NFTs
+                        </div>
+                    </Link>
+
+                    <Link to='/community'>
+                        <div
+                            className='dropdown-link'
+                            onClick={toggleDropdown}
+                        >
+                            Community
+                        </div>
+                    </Link>
+                </div>
+
+            }
+        </>
     )
 }
 
