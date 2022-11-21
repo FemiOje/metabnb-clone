ff19a1958499553b1ac5a13d0fc63b210021d4f0
Oluwafemi Ojetokun <ojetokunakinwale@gmail.com>
2022-11-21 15:31:04 +0100

    fix: card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,28 +5,30 @@ import '../styles/Card.css';
 
 const Card = (props) => {
   return (
-    <div className='card'>    
-        <img 
-          src={props.image} 
-          alt="desert-king"
-          className='card-image'
-        />
-        <br />
-        <br />
-      
+    <div className='card'>
+      <img
+        src={props.image}
+        alt="desert-king"
+        className='card-image'
+      />
+      <br />
+      <br />
+
       <div className="first-row">
-        <small className='title'>Desert King</small>
-        <br />
-        <small className='price'>1MBT per night</small>
+        <span>
+          <small className='title'>Desert King</small>
+          <small className='price'>1MBT per night</small>
+        </span>
       </div>
 
       <br />
       <br />
-      
+
       <div className="second-row">
-        <small className="distance">2345km away</small>
-        <br />
-        <small className="duration">available for 2 weeks stay</small>
+        <span>
+          <small className="distance">2345km away</small>
+          <small className="duration">2weeks stay</small>
+        </span>
       </div>
 
       <br />
